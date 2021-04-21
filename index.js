ba538b474c624bfa77e2ea5d19c32b3cf946a6fa
katekosm <kat.kosmynina@mail.ru>
2021-04-21 21:23:20 +0300

    finished the assignment

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
 const yearElement = document.querySelector("#year");
 yearElement.innerText = new Date().getFullYear();
-
-const dayName = 'Wednesday';
-const monthName = 'April';
-const year = '2021';
-const d = new Date();
-
-const fulldate = `${dayName}, ${d.getDate()}, ${monthName} ${year}`;
