f78eb7b00c7b0259679def53b8e8f8872c24bb6f
katekosm <kat.kosmynina@mail.ru>
2021-04-21 15:01:23 +0300

    made some changes

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const yearElement = document.querySelector("#year");
 yearElement.innerText = new Date().getFullYear();
+
+const dayName = 'Wednesday';
+const monthName = 'April';
+const year = '2021';
+const d = new Date();
+
+const fulldate = `${dayName}, ${d.getDate()}, ${monthName} ${year}`;
