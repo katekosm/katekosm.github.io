ac2de677a27faf5c474c4e498244557124a38b4c
katekosm <kat.kosmynina@mail.ru>
2021-05-20 22:28:59 +0300

    +

--- lesson 5/js/date.js
+++
@@ -1,2 +0,0 @@
-const yearElement = document.querySelector("#date");
-yearElement.innerText = new Date().toLocaleDateString('en-GB', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
--- lesson 5/js/script.js
+++ lesson 5/js/script.js
@@ -1,4 +1,22 @@
+// for current date in the footer
+const yearElement = document.querySelector("#date");
+yearElement.innerText = new Date().toLocaleDateString('en-GB', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+
+
+// for the banner
+const banner = document.querySelector('.banner');
+const dayOfWeek = new Date().getDay();
+if (dayOfWeek === 5) {
+    banner.classList.add('visible');
+}
+
+
+
+// for the menu
 function toggleMenu() {
     // console.log(document.getElementById("primaryNav").class);
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+
+
