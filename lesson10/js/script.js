0757fc05e33de19699fd7931bbd861c475b354ad
katekosm <kat.kosmynina@mail.ru>
2021-06-21 13:06:05 +0300

    +

---
+++ lesson10/js/script.js
@@ -0,0 +1,53 @@
+// for the banner
+const banner = document.querySelector('.banner');
+const dayOfWeek = new Date().getDay();
+if (dayOfWeek === 5) {
+    banner.classList.add('visible');
+}
+
+
+
+// for the menu
+function toggleMenu() {
+    // console.log(document.getElementById("primaryNav").class);
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+// for current date in the footer
+
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+let infoDate = new Date();
+let string = document.lastModified;
+let dayName = days[infoDate.getDay()];
+let monthName = months[infoDate.getMonth()];
+let year = infoDate.getFullYear();
+let fullDate = `${dayName}, ${infoDate.getDate()} ${monthName} ${year}.`;
+let lUpdated = `Last Update: ${string}.`;
+
+
+
+document.querySelector("#currentdate").textContent = fullDate;
+document.querySelector("#lastupdated").textContent = lUpdated;
+document.querySelector("#currentyear").textContent = year;
