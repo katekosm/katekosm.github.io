b5eda0ba03477813f27d542fbd24ade81468670b
katekosm <kat.kosmynina@mail.ru>
2021-06-26 23:58:03 +0300

    +-

--- lesson10/js/script.js
+++ lesson10/js/script.js
@@ -51,3 +51,40 @@ let lUpdated = `Last Update: ${string}.`;
 document.querySelector("#currentdate").textContent = fullDate;
 document.querySelector("#lastupdated").textContent = lUpdated;
 document.querySelector("#currentyear").textContent = year;
+
+
+// for the current weather summary
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=0dcb04ddb341c8d129b99e8b866b705f";
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((town) => {
+        // console.log(town);
+        let description = town.weather[0].description;
+        document.getElementById('currently').innerHTML = description.charAt(0).toUpperCase() + description.slice(1);
+        document.getElementById('temp').innerHTML = Math.round(town.main.temp);
+        document.getElementById('windchill').innerHTML = Math.round(town.main.temp);
+        document.getElementById('humidity').innerHTML = town.main.humidity;
+        document.getElementById('windspeed').innerHTML = Math.round(town.wind.speed);
+    });
+
+// 5 day forecast
+
+const apiURL_forecast = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=0dcb04ddb341c8d129b99e8b866b705f"
+
+fetch(apiURL_forecast)
+    .then(response => response.json())
+    .then((jsObject) => {
+        const forecastData = jsObject.list.filter((element) => element.dt_txt.includes('18:00:00'));
+        const weekdays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+
+        let day = 0;
+        forecastData.forEach(forecast => {
+            let x = new Date(forecast.dt_txt);
+            document.getElementById('temp' + (day + 1)).textContent = Math.round(forecast.main.temp) + ' °F';
+            document.getElementById('img' + (day + 1)).src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
+            document.getElementById('img' + (day + 1)).alt = forecast.weather[0].description
+            document.getElementById('day' + (day + 1)).textContent = weekdays[x.getDay()];
+            day++;
+        });
+    });
--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,9 +1,10 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=0dcb04ddb341c8d129b99e8b866b705f"
+// const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=0dcb04ddb341c8d129b99e8b866b705f"
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=da923bb8ec61575a7dfe5e3106bb43c1"
 
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
+        // console.log(jsObject);
         document.getElementById('current-temp').textContent = jsObject.main.temp;
         const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
         const desc = jsObject.weather[0].description; // note how we reference the weather array
