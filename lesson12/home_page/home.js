54166d02379a8a1e0fcff004726bb2a80c0cc813
katekosm <kat.kosmynina@mail.ru>
2021-07-13 13:07:05 +0300

    lesson12

---
+++ lesson12/home_page/home.js
@@ -0,0 +1,58 @@
+//for the menu
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+// for the current weather summary
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=498817&units=metric&APPID=3689d576134d2fb6825e2a57edc680dc";
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((town) => {
+        let description = town.weather[0].description;
+        document.getElementById('currently').innerHTML = description.charAt(0).toUpperCase() + description.slice(1);
+        document.getElementById('temp').innerHTML = Math.round(town.main.temp);
+        document.getElementById('humidity').innerHTML = town.main.humidity;
+    });
+
+
+// 3 day forecast
+
+const apiURL_forecast = "https://api.openweathermap.org/data/2.5/forecast?id=498817&units=metric&APPID=3689d576134d2fb6825e2a57edc680dc"
+
+fetch(apiURL_forecast)
+    .then(response => response.json())
+    .then((jsObject) => {
+        const forecastData = jsObject.list.filter((element) => element.dt_txt.includes('12:00:00'));
+        const weekdays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+
+        let day = 0;
+        forecastData.forEach(forecast => {
+            let x = new Date(forecast.dt_txt);
+            document.getElementById('temp' + (day + 1)).textContent = Math.round(forecast.main.temp) + '°C';
+            document.getElementById('img' + (day + 1)).src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
+            document.getElementById('img' + (day + 1)).alt = forecast.weather[0].description
+            document.getElementById('day' + (day + 1)).textContent = weekdays[x.getDay()];
+            day++;
+        });
+    });
+
+// const URL_events = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+// fetch(URL_events)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(jsonObject) {
+//         const towns = jsonObject['towns'];
+//         for (let i = 0; i < towns.length; i++) {
+//             if (towns[i].name == 'Soda Springs') {
+//                 let events = towns[i].events;
+//                 for (let i = 0; i < events.length; i++) {
+//                     let event = document.createElement('p');
+//                     event.innerHTML = events[i];
+//                     document.querySelector('.events').appendChild(event);
+//                 }
+//             }
+//         }
+//     });
