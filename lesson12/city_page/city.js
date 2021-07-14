d4a0e00897a193f01c27a6ab8f746308687a9777
katekosm <kat.kosmynina@mail.ru>
2021-07-14 08:44:50 +0300

    +

--- lesson12/city_page/city.js
+++ lesson12/city_page/city.js
@@ -3,41 +3,6 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 
-// for the current weather summary
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=498817&units=metric&APPID=3689d576134d2fb6825e2a57edc680dc";
-
-fetch(apiURL)
-    .then((response) => response.json())
-    .then((town) => {
-        let description = town.weather[0].description;
-        document.getElementById('currently').innerHTML = description.charAt(0).toUpperCase() + description.slice(1);
-        document.getElementById('temp').innerHTML = Math.round(town.main.temp);
-        document.getElementById('humidity').innerHTML = town.main.humidity;
-    });
-
-
-// 3 day forecast
-
-const apiURL_forecast = "https://api.openweathermap.org/data/2.5/forecast?id=498817&units=metric&APPID=3689d576134d2fb6825e2a57edc680dc"
-
-fetch(apiURL_forecast)
-    .then(response => response.json())
-    .then((jsObject) => {
-        const forecastData = jsObject.list.filter((element) => element.dt_txt.includes('12:00:00'));
-        const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-        let day = 0;
-        forecastData.forEach(forecast => {
-            let x = new Date(forecast.dt_txt);
-            document.getElementById('temp' + (day + 1)).textContent = Math.round(forecast.main.temp) + '°F';
-            document.getElementById('img' + (day + 1)).src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
-            document.getElementById('img' + (day + 1)).alt = forecast.weather[0].description
-            document.getElementById('day' + (day + 1)).textContent = weekdays[x.getDay()];
-            if (day == 2) return true;
-            day++;
-        });
-    });
-
 
 const URL_events = "https://katekosm.github.io/lesson12/home_page/city.json";
 
