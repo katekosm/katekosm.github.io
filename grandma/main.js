084871498abd41f839acf5fbc989bbf57153d98c
katekosm <kat.kosmynina@mail.ru>
2021-09-30 23:04:56 +0300

    +

--- grandma/main.js
+++ grandma/main.js
@@ -2,3 +2,20 @@
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+
+$(function() {
+    $('.scrollup').click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    })
+})
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+        $('.scrollup').fadeIn();
+    } else {
+        $('.scrollup').fadeOut();
+    }
+});
